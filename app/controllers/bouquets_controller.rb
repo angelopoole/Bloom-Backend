b772d86db626ee19117cbe178f85f7fce552deaf
angelopoole <angelo.poole@outlook.com>
2020-02-12 09:04:22 -0500

    DFN

--- app/controllers/bouquets_controller.rb
+++ app/controllers/bouquets_controller.rb
@@ -13,8 +13,16 @@ class BouquetsController < ApplicationController
     end
 
     def create
-        bouquet = Bouquet.create(name: params[:name], description: params[:description])
+        bouquet = Bouquet.create(name: params[:name], description: params[:description]) 
+        # byebug
+        # forEach FlowerBouquet.create()
+        # for each id, params[:flower_id], create new FlowerBouquet w/ flowerId - where do i get the flower id from, 
+        params[:flower].each do |element| 
+            # here we take each element "flower_id" and then create a relation using FlowerBouquet.create!
+            flowerBouquet = FlowerBouquet.create(flower_id: params[:flower])
+        end
 
+        
         render json: bouquet
     end
 
